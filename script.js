b5c3fe447131edb2a9c66842574a11181a6b0d29
John Mark Goodson <john.mark.hat@gmail.com>
2020-09-01 14:23:56 -0400

    brush sizing now modifies

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 let canvas = document.getElementById("mycanvas");
 let context = canvas.getContext("2d");
 
+// get colors div for color selection
 let colors = document.querySelector(".colors");
 
+// Get brush size elements
+let shrinkBrush = document.getElementById("shrink-brush");
+let growBrush = document.getElementById("grow-brush");
+let brushSize = document.getElementById("brush-size");
+
+// set default brush
 let brush = {
   size: 5,
   color: "#FF0000"
 };
-
-context.fillStyle = "#FF0000";
-context.fillRect(0, 0, 150, 75);
 
 // When true, moving the mouse draws on the canvas
 let isDrawing = false;
@@ -19,12 +23,10 @@ let y = 0;
 
 // Add the event listeners for mousedown, mousemove, and mouseup
 canvas.addEventListener("mousedown", e => {
-  console.log(e);
   x = e.offsetX;
   y = e.offsetY;
   isDrawing = true;
 });
-
 canvas.addEventListener("mousemove", e => {
   if (isDrawing === true) {
     draw(e);
@@ -32,7 +34,6 @@ canvas.addEventListener("mousemove", e => {
     y = e.offsetY;
   }
 });
-
 canvas.addEventListener("mouseup", e => {
   if (isDrawing === true) {
     draw(e);
@@ -42,7 +43,7 @@ canvas.addEventListener("mouseup", e => {
   }
 });
 
-// use draw for large line
+// Use draw() to generate lines
 function draw(e) {
   var rect = canvas.getBoundingClientRect();
   var posx = e.clientX - rect.left;
@@ -56,6 +57,34 @@ function draw(e) {
 
 // Color Selection
 colors.addEventListener("click", function(e) {
-  console.log(e.target.id);
   brush.color = e.target.id;
 });
+
+// Brush size selection
+brushSize.value = brush.size;
+function displayBrushSize() {
+  brushSize.value = brush.size;
+}
+shrinkBrush.addEventListener("click", function() {
+  if (brush.size > 1) {
+    brush.size -= 1;
+  }
+  displayBrushSize();
+});
+growBrush.addEventListener("click", function() {
+  if (brush.size < 200) {
+    brush.size += 1;
+  }
+  displayBrushSize();
+});
+brushSize.addEventListener("change", function() {
+  // Set max size and multiply value to handle string/number integration
+  if (brushSize.value < 200) {
+    brush.size = brushSize.value * 1;
+  } else {
+    brushSize.value = 200;
+    brush.size = brushSize.value * 1;
+  }
+});
+
+console.log(brushSize);
