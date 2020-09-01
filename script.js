adb76c91e8c1acfb383ab3b40f302832eb9e61fd
John Mark Goodson <john.mark.hat@gmail.com>
2020-09-01 14:02:09 -0400

    stylized colors and added html for size inputs

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// get canvas and context
 let canvas = document.getElementById("mycanvas");
 let context = canvas.getContext("2d");
 
+let colors = document.querySelector(".colors");
+
 let brush = {
-  size: 20,
+  size: 5,
   color: "#FF0000"
 };
-
-console.log(canvas);
 
 context.fillStyle = "#FF0000";
 context.fillRect(0, 0, 150, 75);
@@ -26,41 +27,35 @@ canvas.addEventListener("mousedown", e => {
 
 canvas.addEventListener("mousemove", e => {
   if (isDrawing === true) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
+    draw(e);
     x = e.offsetX;
     y = e.offsetY;
-    drawLarge(e);
   }
 });
 
 canvas.addEventListener("mouseup", e => {
   if (isDrawing === true) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
+    draw(e);
     x = 0;
     y = 0;
     isDrawing = false;
   }
 });
 
-// use draw line for 5px and under
-function drawLine(context, x1, y1, x2, y2) {
-  //   context.beginPath();
-  //   context.strokeStyle = "black";
-  //   context.lineWidth = 20;
-  //   context.moveTo(x1, y1);
-  //   context.lineTo(x2, y2);
-  //   context.stroke();
-  //   context.closePath();
-}
-
 // use draw for large line
-function drawLarge(e) {
+function draw(e) {
   var rect = canvas.getBoundingClientRect();
   var posx = e.clientX - rect.left;
   var posy = e.clientY - rect.top;
 
-  context.fillStyle = "#000000";
+  context.fillStyle = brush.color;
   context.beginPath();
-  context.arc(posx, posy, 25, 0, 2 * Math.PI);
+  context.arc(posx, posy, brush.size, 0, 2 * Math.PI);
   context.fill();
 }
+
+// Color Selection
+colors.addEventListener("click", function(e) {
+  console.log(e.target.id);
+  brush.color = e.target.id;
+});
